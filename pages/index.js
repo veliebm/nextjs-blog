58e6acc44e1e45209f3615302341a9787059213b
Ben <veliebm@gmail.com>
2022-02-20 08:40:57 -0500

    Change desc to be real

--- pages/index.js
+++ pages/index.js
@@ -22,10 +22,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>hey its me ben velie</p>
-        <p>
-          The official blog of me. Testing.
-        </p>
+        <p>The official blog of me.</p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
